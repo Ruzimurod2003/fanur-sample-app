14693771e79348558d452c9654beff3f404d2537
Rozimurod Abdunazarov <ruzimurodabdunazarov2003@mail.ru>
2024-09-20 16:21:32 +0500

    added new logics for index and another pages

---
+++ ViewModels/ContactInfoVM.cs
@@ -0,0 +1,7 @@
+public class ContactInfoVM
+{
+    public string IconClass { get; set; }
+    public string TitleKey { get; set; }
+    public string Content { get; set; }
+    public string LinkPrefix { get; set; }
+}

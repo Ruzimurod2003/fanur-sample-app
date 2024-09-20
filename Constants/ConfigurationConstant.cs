f37d074752e19acf8cc42a7203d0f198aef09f9d
Rozimurod Abdunazarov <ruzimurodabdunazarov2003@mail.ru>
2024-09-20 15:48:28 +0500

    change About Us to cleaning

--- Constants/ConfigurationConstant.cs
+++ Constants/ConfigurationConstant.cs
@@ -1,7 +1,7 @@
 namespace FanurApp.Constants;
 public static class ConfigurationConstant
 {
-    public static string TelegramLink = "https://t.me/fanureducation";
+    public static string TelegramLink = "https://t.me/fanur_administration";
     public static string InstagramLink = "https://www.instagram.com/the_fanur_group";
     public static string TelephoneNumber = "+998 93 322 56 89";
     public static string EmailAddres = "info@fanur.uz";
---
+++ ViewModels/TeamMemberVM.cs
@@ -0,0 +1,8 @@
+namespace FanurApp.ViewModels;
+
+public class TeamMemberVM
+{
+    public string NameKey { get; set; }
+    public string PositionKey { get; set; }
+    public string ImagePath { get; set; }
+}

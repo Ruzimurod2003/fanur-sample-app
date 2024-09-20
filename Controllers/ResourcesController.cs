1d5add6ae97b680c6a7cc7fc438ea09efea1c1d0
Rozimurod Abdunazarov <ruzimurodabdunazarov2003@mail.ru>
2024-09-20 13:10:05 +0500

    added new changes for resources and another files

---
+++ Controllers/ResourcesController.cs
@@ -0,0 +1,156 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FanurApp.Data;
+using FanurApp.Models;
+
+namespace FanurApp.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public ResourcesController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Resources
+        public async Task<IActionResult> Index()
+        {
+            var applicationContext = _context.Resources.Include(r => r.Culture);
+            return View(await applicationContext.ToListAsync());
+        }
+
+        // GET: Resources/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources
+                .Include(r => r.Culture)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return View(resource);
+        }
+
+        // GET: Resources/Create
+        public IActionResult Create()
+        {
+            ViewData["CultureId"] = new SelectList(_context.Cultures, "Id", "Name");
+            return View();
+        }
+
+        // POST: Resources/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Key,Value,CultureId")] Resource resource)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(resource);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CultureId"] = new SelectList(_context.Cultures, "Id", "Name", resource.CultureId);
+            return View(resource);
+        }
+
+        // GET: Resources/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources.FindAsync(id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+            ViewData["CultureId"] = new SelectList(_context.Cultures, "Id", "Name", resource.CultureId);
+            return View(resource);
+        }
+
+        // POST: Resources/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Key,Value,CultureId")] Resource resource)
+        {
+            if (id != resource.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(resource);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ResourceExists(resource.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CultureId"] = new SelectList(_context.Cultures, "Id", "Name", resource.CultureId);
+            return View(resource);
+        }
+
+        // GET: Resources/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources
+                .Include(r => r.Culture)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return View(resource);
+        }
+
+        // POST: Resources/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var resource = await _context.Resources.FindAsync(id);
+            if (resource != null)
+            {
+                _context.Resources.Remove(resource);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ResourceExists(int id)
+        {
+            return _context.Resources.Any(e => e.Id == id);
+        }
+    }
+}

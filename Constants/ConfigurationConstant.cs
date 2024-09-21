e995cbf35c13d819f39c348ac3b0a9d484ffa5b3
Rozimurod Abdunazarov <ruzimurodabdunazarov2003@mail.ru>
2024-09-21 16:50:15 +0500

    added new changes for chnage partners and phone number

--- Constants/ConfigurationConstant.cs
+++ Constants/ConfigurationConstant.cs
@@ -3,8 +3,10 @@ public static class ConfigurationConstant
 {
     public static string TelegramLink = "https://t.me/fanur_administration";
     public static string InstagramLink = "https://www.instagram.com/the_fanur_group";
-    public static string TelephoneNumber = "+998 93 322 56 89";
+    public static string TelephoneNumber = "+998 93 322 56 89";    
+    public static string TelephoneNumberFirst = "+998 93 322 56 89";    
+    public static string TelephoneNumberSecond = "+998 97 752 17 27";
     public static string EmailAddres = "info@fanur.uz";
-    public static string LocationAddres = "Toshkent shahri, Olmazor tumani, Talabalar shaharchasi, Inno Technopark, Postcode: 100073";
+    public static string LocationAddres = "Tashkent city, Olmazor district, Talabalar street, 2";
     public static string ProjectTitle = "Fanur Group";
 }
--- ViewModels/ContactInfoVM.cs
+++ ViewModels/ContactInfoVM.cs
@@ -3,5 +3,6 @@ public class ContactInfoVM
     public string IconClass { get; set; }
     public string TitleKey { get; set; }
     public string Content { get; set; }
+    public List<string> ContentList { get; set; }
     public string LinkPrefix { get; set; }
 }
